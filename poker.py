37b7969f1da9af0e20707d014c2e0b946e3b4853
Santa-Claws <theincredibletristan@gmail.com>
2022-05-29 14:43:48 -0700

    need betting fiuxed everything else

--- poker.py
+++ poker.py
@@ -32,7 +32,7 @@ def is_royal_flush(cards: List[Card]) -> Tuple:
 
 
 def is_flush(cards: List[Card]) -> Tuple:
-    for suit in Deck.suits:
+    for suit in Deck.suits.values():
         if all([card.suit == suit for card in cards]):
             return sorted_by_value(cards)
     return ZERO_SCORE
@@ -71,8 +71,8 @@ def is_two_pair(cards: List[Card]) -> Tuple:
         (score_2, da_other_card_name) = is_n_of_kind(remaining_cards, 2)
         if score_2[0] != 0:
             ordered_scores = score_2[0:2] + score[0:2] if score_2[0] > score[0] else score[0:2] + score_2[0:2]
-            remaining_cards = [card for card in cards if card.name != da_other_card_name]
-            return ordered_scores + tuple(remaining_cards)
+            remaining_cards = [card for card in remaining_cards if card.name != da_other_card_name]
+            return ordered_scores + tuple([card.value for card in remaining_cards])
         else:
             return ZERO_SCORE
     else:
@@ -94,32 +94,35 @@ def high_card(cards: List[Card]) -> Tuple:
 
 def find_winner(players: List[Player]) -> Player:
     # players[0].cards
-
-    # returns (combo_type, score) tuple
+    import functools
+    list_of_functions = [
+        (is_royal_flush, "royal flush"),
+        (is_strait_flush, "straight flush"),
+        (is_strait, "straight"),
+        (is_flush, "flush"),
+        (functools.partial(is_n_of_kind, n=4), "4 of a kind"),
+        (functools.partial(is_n_of_kind, n=3), "3 of a kind"),
+        (is_two_pair, "two pair"),
+        (functools.partial(is_n_of_kind, n=2), "pair"),
+        (high_card, "high card")
+    ]
+    # returns (combo_type, scores) tuple
     def find_combo_type(cards: List[Card]) -> Tuple:
-        import functools
-        list_of_functions = [
-            is_royal_flush,
-            is_strait_flush,
-            is_strait,
-            is_flush,
-            functools.partial(is_n_of_kind, n=4),
-            functools.partial(is_n_of_kind, n=3),
-            is_two_pair,
-            functools.partial(is_n_of_kind, n=2),
-            high_card
-        ]
-        for count, function in enumerate(list_of_functions):
+        for count, function in enumerate([f[0] for f in list_of_functions]):
             score = function(cards=cards)
             score = score[0] if isinstance(score[0], Tuple) else score
             if score[0]:
                 # the combo type is descending values
                 return (len(list_of_functions) - count,) + score
 
+    combo_name_lookup = {
+        (len(list_of_functions) - combo[0]): combo[1][1]
+        for combo in enumerate(list_of_functions)
+    }
     player_combos = []
     for player in players:
         combo_type = find_combo_type(player.cards)
-        print(f"Player: {player.name}, combo: {combo_type}")
+        print(f"Player: {player.name}, combo: {combo_name_lookup[combo_type[0]]} {combo_type}")
         player_combos.append((combo_type, player))
 
     player_combos_sorted = sorted(player_combos, reverse=True)
