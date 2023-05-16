1addd8d48613ff370ffb6598143aad83841286d7
Santa-Claws <75145758+Santa-Claws@users.noreply.github.com>
2023-05-15 21:46:02 -0700

    Update war.py

--- war.py
+++ war.py
@@ -2,10 +2,21 @@ from deck import Deck
 from player import Player
 
 
-def play_round(players, is_war=False):
-    round = [(player, player.cards.pop(0)) for player in players]
+def play_round(players, num_rounds, war_rounds, is_war=False):
+    
+    try:
+        round = [(player, player.cards.pop(0)) for player in players]
+        # draws first card in player cards
+    exept IndexError:
+        end_o_game(players, num_rounds, war_rounds)
+        exit()
+        # checks if player runs out of cards
+        # meant for WAR rounds
+        
     played_cards = [card[1] for card in round]
     print("Round: ", round)
+    # adds played cards to player_cards and print round info
+    
     if is_war:
         print('Its war..', round)
         return None, played_cards
@@ -13,10 +24,17 @@ def play_round(players, is_war=False):
     if tie:
         print("Tie: ", round, " This means WAR!")
         return None, played_cards
+        # meant if it is war, sends war back to main
+    
     winner = max(round, key=lambda p: p[1].value)
     print("Winner: ", winner[0].name, winner[1])
     return winner[0].name, played_cards
+    # if normal round then calcs winner and sends back to main
 
+def end_o_game(players, num_rounds, war_rounds):
+    print(f'Game Over ... in {num_rounds} with {war_rounds} WAR rounds')
+    [print(player, player.cards) for player in players]
+    # just gives game info and ends game
 
 if __name__ == "__main__":
     deck = Deck()
@@ -28,6 +46,8 @@ if __name__ == "__main__":
         for i in range(0, len(player.cards), 13):
             [print(f"{str(card)}, ", end="") for card in player.cards[i:i + 13]]
             print()
+         
+        # prints player cards and card info
 
     cards_on_table = []
     num_rounds, war_rounds = 0, 0
@@ -35,9 +55,13 @@ if __name__ == "__main__":
         [print(f"{player.name:10}", [card for card in player.cards]) for player in players]
         winner, played_cards = play_round(players)
         cards_on_table.extend(played_cards)
+        # prints cards and card info
+        
         if winner:
             list(filter(lambda p: p.name == winner, players))[0].cards.extend(cards_on_table)
             cards_on_table.clear()
+            # checks if normal rounds and proceeds with game flow
+            
         else:
             for _ in range(3):
                 _, additional_cards = play_round(players, is_war=True)
@@ -46,5 +70,7 @@ if __name__ == "__main__":
         print()
         num_rounds += 1
         #sleep(1)
-    print(f'Game over... in {num_rounds} rounds with {war_rounds} WAR rounds\n')
-    [print(player, player.cards) for player in players]
+       
+     # if reaches this code then one player has run out of cards and game is over
+     end_o_game(players, num_rounds, war_rounds)
+    
